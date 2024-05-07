88373b22ca31255d1b14f5e3c13d12685b6ee1ed
ranjanmangla1 <ranjanmangla1@gmail.com>
2024-05-07 22:50:59 +0530

    completed step 4

---
+++ tests/step-04/index.test.js
@@ -0,0 +1,11 @@
+const executeSELECTQuery = require('../../src/index');
+
+test('Execute SQL Query', async () => {
+    const query = 'SELECT id, name FROM sample';
+    const result = await executeSELECTQuery(query);
+    expect(result.length).toBeGreaterThan(0);
+    expect(result[0]).toHaveProperty('id');
+    expect(result[0]).toHaveProperty('name');
+    expect(result[0]).not.toHaveProperty('age');
+    expect(result[0]).toEqual({ id: '1', name: 'John' });
+});
